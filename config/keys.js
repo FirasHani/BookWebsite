e244108b36811edf54326346bdc267b09dad9b15
FirasHani <77682961+FirasHani@users.noreply.github.com>
2022-11-10 11:23:49 +0200

    Delete config directory

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-dbPassword = 'mongodb+srv://FirasHussein:'+ encodeURIComponent('Hesoyam') + '@cluster0.9zlt4dj.mongodb.net/Nour?retryWrites=true';
-
-module.exports = {
-    mongoURI: dbPassword
-};
